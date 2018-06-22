94f48119b37673c535026192748852d1b4f80544
随风 <suifengin412@gmail.com>
2018-06-22 14:08:33 +0800

    [add] 增加返回数据类

---
+++ core/Response.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace core;
+
+class Response
+{
+    
+    private static $content = null;
+
+    /**
+     * 设置json返回格式
+     * @param $data
+     */
+    public static function setJsonContent($data)
+    {
+        header('Content-type: application/json');
+        self::$content = json_encode($data);
+    }
+
+
+    public static function send()
+    {
+        if(!empty(self::$content)){
+            echo self::$content;
+        }else{
+            echo json_encode(
+                [
+                    'code' => -1,
+                    'errorMsg' => 'system error',
+                    'msg' => 'unknown error in server'
+                ]
+            );
+        }
+        die();
+    }
+
+
+
+}
+
+
+
+
+
